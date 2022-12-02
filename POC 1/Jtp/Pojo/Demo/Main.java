2737372715a1380576bb93b8f9b5eee1b563f591
Invenio-Trideep <116712458+Invenio-Trideep@users.noreply.github.com>
2022-12-02 11:43:50 +0530

    Update Main.java

--- POC 1/Jtp/Pojo/Demo/Main.java
+++ POC 1/Jtp/Pojo/Demo/Main.java
@@ -12,41 +12,41 @@ public class Main
         Scanner sc = new Scanner(new File("read-file-java-poc1.csv"));
         String delimiter=" ; "; 
         String s; 
-        List<List<String>> lines = new ArrayList();
+        HashMap<String,List<Lines>> hm=new HashMap<String,List<Lines>>();
         // sc.useDelimiter(";");
         while (sc.hasNext())
         {  
             s=sc.next();
             String[] sArr = s.split(";", 0);
             // System.out.println(sArr);
-if (s.startsWith("HAUDIT")){
-    Header headerItem = new Header();
-    headerItem.setrecordType(sArr[0]);
-    headerItem.settotalLines(Integer.parseInt(sArr[1]));
-    headerItem.settotalAmt(Float.parseFloat(sArr[2]));
-    headerItem.setminAmt(Integer.parseInt(sArr[3]));
-    headerItem.setcurrentDate(sArr[4]);
-    System.out.println("Header: "+headerItem);
-}
-HashMap<String,Object> hm=new HashMap<String,Object>();
-if (s.startsWith("LINE")){
-    Lines linesItem = new Lines();
-    linesItem.setrecordType(sArr[0]);
-    linesItem.setlineNumber(Integer.parseInt(sArr[1]));
-    linesItem.setrefDocNum(sArr[2]);
-    linesItem.setcity(sArr[3]);
-    linesItem.setamount(Float.parseFloat(sArr[4]));
-    hm.put(sArr[2],linesItem);
-    for(Map.Entry k : hm.entrySet()){    
-        System.out.println(k.getKey()+"=["+"LineItems"+k.getValue()+"]");    
-       }  
+            if (s.startsWith("HAUDIT")){
+                Header headerItem = new Header();
+                headerItem.setrecordType(sArr[0]);
+                headerItem.settotalLines(Integer.parseInt(sArr[1]));
+                headerItem.settotalAmt(Float.parseFloat(sArr[2]));
+                headerItem.setminAmt(Integer.parseInt(sArr[3]));
+                headerItem.setcurrentDate(sArr[4]);
+                System.out.println("Header: "+headerItem);
+            }
 
-}
+            if (s.startsWith("LINE")){
+                Lines linesItem = new Lines();
+                linesItem.setrecordType(sArr[0]);
+                linesItem.setlineNumber(Integer.parseInt(sArr[1]));
+                linesItem.setrefDocNum(sArr[2]);
+                linesItem.setcity(sArr[3]);
+                linesItem.setamount(Float.parseFloat(sArr[4]));
+                if (hm.get(sArr[2])==null) {
+                    List<Lines> linesList = new ArrayList<Lines>();
+                    linesList.add(linesItem);
+                    hm.put(sArr[2],linesList);
+                } else {
+                    hm.get(sArr[2]).add(linesItem);
+                }
+            }
 
-            List values = Arrays.asList(s.split(delimiter));
-            lines.add(values);
-        } 
-        // lines.forEach(l -> System.out.println(l));  
+        }
+        System.out.println("HashMap: "+hm);
         sc.close();
     }
 }
